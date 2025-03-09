b2cc137c9102c9b7a6123542c428b8ca13670f32
Arafat2342 <jashoresinger@gmail.com>
2025-03-09 19:38:22 +0600

    happy birthday

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 print('think you have 5 friends and you have to write everyones birthday')
 
-first_friend = int(input('birthday of your 1st friend date: '))
-second_friend = int(input('birthday of your 2nd friend date: '))
-third_friend = int(input('birthday of your 3rd friend date: '))
-forth_friend = int(input('birthday of your 4th friend date: '))
-fifth_friend = int(input('birthday of your 5th friend date: '))
+first_friend = input('birthday of your 1st friend date: ')
+second_friend = input('birthday of your 2nd friend date: ')
+third_friend = input('birthday of your 3rd friend date: ')
+forth_friend = input('birthday of your 4th friend date: ')
+fifth_friend = input('birthday of your 5th friend date: ')
 
 print('birthday of your 1st friend: ',first_friend,'\nbirthday of your 2nd friend: ',second_friend,'\nbirthday of your 3rd friend: ',third_friend,'\nbirthday of your 4th friend: ',forth_friend,'\nbirthday of your 5th friend: ',fifth_friend)
